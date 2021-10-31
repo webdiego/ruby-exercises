6b01d122a572cc2734b484574cce4904120f38e6
webdiego <webdiegomassarini@gmail.com>
2021-10-31 10:14:39 +0100

    first lession

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -1,36 +1,47 @@
 def add(a, b)
   # return the result of adding a and b
-  a + b
+   puts a + b
 end
 
 def subtract(a, b)
-  # return the result of subtracting b from a
+  puts b - a
 end
 
 def multiply(a, b)
-  # return the result of multiplying a times b
+  puts a * b
 end
 
 def divide(a, b)
   # return the result of dividing a by b
+  puts a / b
 end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+  a % b
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  puts a / b.to_f
 end
 
 def string_to_number(string)
   # return the result of converting a string into an integer
+  puts string.to_i
 end
 
 def even?(number)
   # return true if the number is even (hint: use integer's even? method)
+  puts number.even?
 end
 
 def odd?(number)
   # return true if the number is odd (hint: use integer's odd? method)
+  puts number.odd?
 end
+
+
+puts "Execute: "
+odd?(2)
+string_to_number("3")
--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -5,36 +5,50 @@ end
 
 def concatenate(string)
   # use concatenation to format the result to be "Hello <string>!"
+  puts "Hello " << string << "!"
 end
 
 def substrings(word)
   # return the first 4 letters from the word using substrings
+  puts word[0..3]
 end
 
 def capitalize(word)
   # capitalize the first letter of the word
+  puts word.capitalize
 end
 
 def uppercase(string)
   # uppercase all letters in the string
+  puts string.upcase
 end
 
 def downcase(string)
   # downcase all letters in the string
+  puts string.downcase
 end
 
 def empty_string(string)
   # return true if the string is empty
+  puts string.empty?
 end
 
 def string_length(string)
   # return the length of the string
+  puts string.lenght
 end
 
 def reverse(string)
   # return the same string, with all of its characters reversed
+  puts string.reverse
 end
 
 def space_remover(string)
   # remove all the spaces in the string using gsub
+  puts string.strip
 end
+
+puts "Exectute"
+
+reverse("buenas")
+space_remover("   Buenas dia  ")
