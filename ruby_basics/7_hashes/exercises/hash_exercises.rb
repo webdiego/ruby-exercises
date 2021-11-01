ae3561c0cc4a6d3fed668b95052fb0562dec74a1
webdiego <webdiegomassarini@gmail.com>
2021-11-01 11:14:12 +0100

    hashes

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -29,21 +29,24 @@ end
 
 def remove_favorite_number(favorite_list)
   # Step 1: delete the number data
-
+  favorite_list.delete("number")
   # Step 2: return the hash (because Step 1 returns the value of the number key)
   favorite_list
 end
 
 def favorite_categories(favorite_list)
   # return the keys of favorite_list
+  favorite_list.keys
 end
 
 def favorite_items(favorite_list)
   # return the values of favorite_list
+  favorite_list.values
 end
 
 def merge_favorites(original_list, additional_list)
   # merge the two hashes: original_list and additional_list
+  original_list.merge(additional_list)
 end
 
 puts "Execute"
@@ -60,6 +63,18 @@ puts favorite_c
 puts "Check if the number is present or return 42"
 favorite_n = favorite_number(my_hash)
 puts favorite_n
-
+puts "Update hash with movie"
 add_movie = update_favorite_movie( my_hash ,"Interstellar")
 puts add_movie
+puts "Delete key of number"
+remove_number  = remove_favorite_number( my_hash )
+puts remove_number
+puts "Return all keys in hash"
+favorite_keys  = favorite_categories( my_hash )
+puts favorite_keys
+puts "Return all values in hash"
+favorite_values  = favorite_items( my_hash )
+puts favorite_values
+puts "Merge 2 hashs"
+merged  = merge_favorites( my_hash , { "city" => "sydney"})
+puts merged
